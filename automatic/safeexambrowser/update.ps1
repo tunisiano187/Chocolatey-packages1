fae9baed19d1e18ba3956a5b5ccb5db3a1082896
Tunisiano18 <tunisiano187@users.noreply.github.com>
2026-03-09 10:59:27 +0000

    correction

--- automatic/safeexambrowser/update.ps1
+++ automatic/safeexambrowser/update.ps1
@@ -1,34 +1,37 @@
-import-module Chocolatey-AU
+import-module chocolatey-AU
+Import-Module ..\..\scripts\au_extensions.psm1
 
-$releases = 'https://github.com/SafeExamBrowser/seb-win-refactoring/releases'
+$releases = 'https://github.com/SafeExamBrowser/seb-win-refactoring/releases/latest'
+$Owner = $releases.Split('/') | Select-Object -Last 1 -Skip 3
+$repo = $releases.Split('/') | Select-Object -Last 1 -Skip 2
 
+function global:au_BeforeUpdate {
+	. ..\..\scripts\Get-FileVersion.ps1
+	$FileVersion = Get-FileVersion $Latest.URL32 -keep
+	Move-Item -Path $FileVersion.TempFile -Destination "tools/airshipper-windows.msi"
+	$Latest.Checksum32 = $FileVersion.Checksum
+	$Latest.ChecksumType32 = $FileVersion.checksumType
+}
 
-function global:au_SearchReplace {
-    @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
-    }
+function global:au_AfterUpdate($Package) {
+  Invoke-VirusTotalScan $Package
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+	$tags = Get-GitHubRelease -OwnerName $Owner -RepositoryName $repo -Latest
+	$url = $tags.assets.browser_download_url | Where-Object {$_ -match ".msi$"}
+    $url32 = $url | Where-Object {$_ -match "x86"}
+    $url64 = $url | Where-Object {$_ -match "x64"}
+	$version = $tags.tag_name.Replace('v','')
+	Update-Metadata -key "releaseNotes" -value $tags.html_url
+	Update-Metadata -key "licenseUrl" -value $((Get-GitHubLicense -OwnerName $Owner -RepositoryName $repo).download_url)
+	if($tags.prerelease -match "true") {
+    	$date = $tags.published_at.ToString("yyyyMMdd")
+    	$version = "$version-pre$($date)"
+	}
 
-    $version = $download_page.Links | ? href -like '*releases/tag*' | ForEach-Object href | select -First 1
-    $version = Split-Path $version -Leaf
-    $download_page = Invoke-WebRequest -Uri ($releases + '/expanded_assets/' + $version)
-    $url64 = $download_page.Links | ? href -match '.x64_Setup.msi$' | ForEach-Object href | select -First 1
-    $url32 = $download_page.Links | ? href -match '.x86_Setup.msi$' | ForEach-Object href | select -First 1
-    $version = (Split-Path ( Split-Path $url32 ) -Leaf).Substring(1)
-
-    @{
-        URL32   = 'https://github.com' + $url32
-        URL64   = 'https://github.com' + $url64
-        Version = $version
-    }
+  	$Latest = @{ URL32 = $url32; Version = $version }
+	return $Latest
 }
 
 update
